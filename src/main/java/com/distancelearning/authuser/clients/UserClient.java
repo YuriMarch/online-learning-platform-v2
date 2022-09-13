faab9020411fa1eb632130f5ac9a3b7c09053ac2
yurimarch <ypdmarch@gmail.com>
2022-09-13 15:14:29 +0200

    .

--- src/main/java/com/distancelearning/authuser/clients/UserClient.java
+++ src/main/java/com/distancelearning/authuser/clients/UserClient.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -27,24 +26,25 @@ public class UserClient {
 
     private final UtilsService utilsService;
 
-    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
+    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDto> searchResult = null;
         String url = utilsService.createUrl(userId, pageable);
 
         log.debug("Request URL: {}", url);
         log.info("Request URL: {}", url);
+        ResponseEntity<ResponsePageDto<CourseDto>> result = null;
         try {
             ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType =
-                    new ParameterizedTypeReference<ResponsePageDto<CourseDto>>(){};
-            ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+                    new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
             searchResult = result.getBody().getContent();
 
             log.debug("Response number of elements: {}", searchResult.size());
-        } catch (HttpStatusCodeException e){
+        } catch (HttpStatusCodeException e) {
 
             log.error("Error request /courses {}", e);
         }
         log.info("Ending request /courses userId {}", userId);
-        return new PageImpl<>(searchResult);
+        return result.getBody();
     }
 }
