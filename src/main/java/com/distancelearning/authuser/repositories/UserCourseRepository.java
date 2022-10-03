65c0c0785926b095bfd5bc0f2848f52830823de6
yurimarch <ypdmarch@gmail.com>
2022-10-04 01:53:36 +0200

    deletion method updated for data consistency.

--- src/main/java/com/distancelearning/authuser/repositories/UserCourseRepository.java
+++ src/main/java/com/distancelearning/authuser/repositories/UserCourseRepository.java
@@ -3,10 +3,16 @@ package com.distancelearning.authuser.repositories;
 import com.distancelearning.authuser.models.User;
 import com.distancelearning.authuser.models.UserCourseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserCourseRepository extends JpaRepository<UserCourseModel, UUID> {
 
     boolean existsByUserAndCourseId(User User, UUID CourseId);
+
+    @Query(value = "select * from tb_users_courses where user_user_id = :userId", nativeQuery = true)
+    List<UserCourseModel> findAllUserCourseIntoUser(@Param("userId") UUID userId);
 }
--- src/main/java/com/distancelearning/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/distancelearning/authuser/services/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.distancelearning.authuser.services.impl;
 
 import com.distancelearning.authuser.models.User;
+import com.distancelearning.authuser.models.UserCourseModel;
+import com.distancelearning.authuser.repositories.UserCourseRepository;
 import com.distancelearning.authuser.repositories.UserRepository;
 import com.distancelearning.authuser.services.UserService;
 import lombok.AllArgsConstructor;
@@ -8,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +21,8 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+
+    private final UserCourseRepository userCourseRepository;
 
     @Override
     public List<User> findAll() {
@@ -34,8 +39,13 @@ public class UserServiceImpl implements UserService {
         return userRepository.findById(userId);
     }
 
+    @Transactional
     @Override
     public void delete(User user) {
+        List<UserCourseModel> userCourseModelList = userCourseRepository.findAllUserCourseIntoUser(user.getUserId());
+        if (!userCourseModelList.isEmpty()){
+            userCourseRepository.deleteAll(userCourseModelList);
+        }
         userRepository.delete(user);
     }
 
