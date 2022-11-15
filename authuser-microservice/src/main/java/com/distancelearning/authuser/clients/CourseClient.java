3654888edf5dea899ead3600609b0dab190adef0
Yuri March <78003803+YuriMarch@users.noreply.github.com>
2022-11-15 18:41:01 +0100

    Add files via upload

---
+++ authuser-microservice/src/main/java/com/distancelearning/authuser/clients/CourseClient.java
@@ -0,0 +1,55 @@
+package com.distancelearning.authuser.clients;
+
+import com.distancelearning.authuser.dtos.CourseDto;
+import com.distancelearning.authuser.dtos.ResponsePageDto;
+import com.distancelearning.authuser.services.UtilsService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@Log4j2
+public class CourseClient {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    UtilsService utilsService;
+
+    @Value("${distancelearning.api.url.course}")
+    String REQUEST_URL_COURSE;
+
+    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
+        List<CourseDto> searchResult = null;
+        String url = REQUEST_URL_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable);
+
+        log.debug("Request URL: {}", url);
+        log.info("Request URL: {}", url);
+        ResponseEntity<ResponsePageDto<CourseDto>> result = null;
+        try {
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType =
+                    new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = result.getBody().getContent();
+
+            log.debug("Response number of elements: {}", searchResult.size());
+        } catch (HttpStatusCodeException e) {
+
+            log.error("Error request /courses {}", e);
+        }
+        log.info("Ending request /courses userId {}", userId);
+        return result.getBody();
+    }
+}
---
+++ authuser-microservice/src/main/java/com/distancelearning/authuser/models/User.java
@@ -0,0 +1,75 @@
+package com.distancelearning.authuser.models;
+
+import com.distancelearning.authuser.dtos.UserEventDto;
+import com.distancelearning.authuser.enums.UserStatus;
+import com.distancelearning.authuser.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+import org.springframework.hateoas.RepresentationModel;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_USERS") //Serializable converts Java objects to a sequence of bytes that can be stored in DB
+public class User extends RepresentationModel<User> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 250)
+    @JsonIgnore
+    private String password;
+
+    @Column(nullable = false, length = 150)
+    private String fullName;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    @Column(length = 20)
+    private String phoneNumber;
+
+    @Column(length = 20)
+    private String cpf;
+
+    @Column
+    private String imageUrl;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime creationDate;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime lastUpdateDate;
+
+    public UserEventDto convertToUserEventDto() {
+        var userEventDto = new UserEventDto();
+        BeanUtils.copyProperties(this, userEventDto);
+        userEventDto.setUserType(this.getUserType().toString());
+        userEventDto.setUserStatus(this.getUserStatus().toString());
+        return userEventDto;
+    }
+}
